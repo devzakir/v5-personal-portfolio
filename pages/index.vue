666c93561978b4fea37ff648c4e5299d7e7b5e91
Zakir Hossen <web.zakirbd@gmail.com>
2022-07-29 18:36:49 +0600

    feat: homepage design added

---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="homepage">
+    <header class="bg-blue-500 bg-opacity-80 sticky top-0 z-10 w-full backdrop-filter backdrop-blur-lg">
+      <div class="container mx-auto">
+        <nav class="flex items-center justify-between py-2">
+          <a href="#" class="logo">
+            <img class="max-w-[150px] max-h-[40px]" src="https://zakirhossen.com/_nuxt/img/logo-white.e9fbf76.png" alt="">
+          </a>
+          <ul class="flex items-center justify-end">
+            <li><a class="px-4 text-white hover:text-gray-900" href="#"> Home </a></li>
+            <li><a class="px-4 text-white hover:text-gray-900" href="#"> Home </a></li>
+            <li><a class="px-4 text-white hover:text-gray-900" href="#"> Home </a></li>
+            <li><a class="px-4 text-white hover:text-gray-900" href="#"> Home </a></li>
+            <li><a class="px-4 text-white hover:text-gray-900" href="#"> Home </a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section class="py-48 bg-white backdrop-filter backdrop-blur-md bg-opacity-90">
+      <div class="container mx-auto">
+        <div class="flex justify-between max-w-5xl mx-auto">
+          <div class="w-80 h-80 shrink-0 mr-20 rounded-full outline-dashed outline-8 outline-offset-8 outline-blue-600 border-2 border-gray-900 overflow-hidden">
+            <img class="w-full h-full object-cover object-top" src="https://api.zakirhossen.com/storage/setting/1616300141.jpg" alt="">
+          </div>
+          <div>
+            <h3 class="text-xl uppercase text-gray-700">
+              👋 Hi, I am Zakir
+            </h3>
+            <div class="content" >
+              <h3 class="text-4xl mb-4"> I am a 22-years-old entrepreneur!</h3>
+              <p class="mb-2">I’m currently dedicating most of my time with my team on 
+                <a class="text-blue-700"  href="https://zakirsoft.com">Zakirsoft</a>
+                building awesome digital products. We are a team of people working hard to build innovative digital products.
+              </p>
+              <p class="mb-2">
+                I am also working on creating <a class="text-blue-700" href="courses"> online courses </a>,
+                I am developing an effective method for learning programming languages. I am creating courses on
+                Laravel, Vue.js, Nuxt.js, and Bootstrap.
+              </p>
+              <p class="mb-2">
+                Before that, I used work in a Japanese company back in 2018. Then I started working as a
+                freelancer working in different marketplaces like 
+                <a class="text-blue-700" href="https://www.fiverr.com/zakir18">
+                Fiverr</a>
+                <a class="text-blue-700" href="https://www.upwork.com/freelancers/~014e565b3a8183bd74">Upwork</a>
+                ! Where I have worked with over 40+ clients.</p>
+              <p class="mb-2">
+                I've also started and worked on many other projects that unfortunately never saw the light
+                of day, like <a class="text-blue-700" href="https://www.facebook.com/joybazarbd/"> Joybazar </a>, Best
+                Hammock Chair, or Podcast.</p>
+              <p>
+                If you've reached this point, you probably want tolearn more about my projects. You can also follow me on 
+                <a class="text-blue-700" href="https://facebook.com/devzakir">
+                  Facebook
+                </a>
+                , where I actively share what I'm working on, or 
+                <a class="text-blue-700" href="https://www.youtube.com/channel/UCtmtIr6waLhrgNmFYHjznvw">
+                  subscribe to my Youtube 
+                </a> 
+                to get regular updates on my projects. 
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
