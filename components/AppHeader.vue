4ea9373170cbc8e35558702c9e7184bf2c9513e1
Zakir Hossen <web.zakirbd@gmail.com>
2022-08-09 23:06:08 +0600

    feat: timeline, project json data added &  polishing UI

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header>
-    <nav class="bg-gray-100 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+  <header class="border-b-gray-200 border-b-[thin] dark:border-b-gray-600">
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
       <div class="max-w-5xl mx-auto">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <nuxt-link to="/" class="flex items-center">
