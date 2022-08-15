36cd0f3d5621d1ec8e3ea2564553787a1c9369fd
Zakir Hossen <web.zakirbd@gmail.com>
2022-08-15 11:41:57 +0600

    content: timeline page content added

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -5,7 +5,7 @@
         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 dark:text-white"> 
           My Career Timeline
         </h1>
-        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 dark:text-gray-400">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 dark:text-gray-400">The only impossible journey is the one you never begin. Life is a journey and every chapter is very interesting </p>
       </div>
     </div>
   </div>
@@ -13,7 +13,7 @@
     <div class="max-w-5xl mx-auto">
       <ol class="ml-5 relative border-l border-gray-200 dark:border-gray-700">
         <li class="mb-10 ml-6" v-for="(item, index) in data.body" :key="index">
-          <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-800 dark:bg-blue-900">
+          <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-gray-100 dark:ring-gray-800 dark:bg-blue-900">
             <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor"
               viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd"
@@ -23,7 +23,7 @@
           </span>
           <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
             {{ item.title }}
-            <!-- <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span> -->
+            <span v-if="item.latest" class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span>
           </h3>
           <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             Released on {{ item.date }}
@@ -31,14 +31,6 @@
           <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             {{ item.description }}
           </p>
-          <!-- <a href="#"  class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
-            <svg  class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
-                clip-rule="evenodd"></path>
-            </svg>
-            Download ZIP
-          </a> -->
         </li>
       </ol>
     </div>
