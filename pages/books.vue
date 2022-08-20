b2ae6e3e4117063b10cd331e317245a3ab9b470f
Zakir Hossen <web.zakirbd@gmail.com>
2022-08-20 19:43:29 +0600

    fix: router loading issue

---
+++ pages/books.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="py-16 bg-gray-100 dark:bg-gray-900 dark:border-b-gray-800 border-b">
+      <div class="max-w-5xl mx-auto">
+        <div class="flex flex-wrap w-full flex-col items-center text-center">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 dark:text-white"> 
+            Books Recommendation
+          </h1>
+          <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 dark:text-gray-400">
+            Books that inspire me to move forward. Books that helps me to become a better businessman and a better a person.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="py-80 bg-white dark:bg-gray-900 dark:border-b-gray-800 border-b">
+      <div class="max-w-5xl mx-auto">
+        <p class="text-center">Coming Soon</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,38 +1,40 @@
 <template>
-  <div class="py-16 bg-gray-100 dark:bg-gray-900 dark:border-b-gray-800 border-b">
-    <div class="max-w-5xl mx-auto">
-      <div class="flex flex-wrap w-full flex-col items-center text-center">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 dark:text-white"> 
-          My Career Timeline
-        </h1>
-        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 dark:text-gray-400">The only impossible journey is the one you never begin. Life is a journey and every chapter is very interesting </p>
-      </div>
+  <div>
+    <div class="py-16 bg-gray-100 dark:bg-gray-900 dark:border-b-gray-800 border-b">
+        <div class="max-w-5xl mx-auto">
+        <div class="flex flex-wrap w-full flex-col items-center text-center">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 dark:text-white"> 
+            My Career Timeline
+            </h1>
+            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 dark:text-gray-400">The only impossible journey is the one you never begin. Life is a journey and every chapter is very interesting </p>
+        </div>
+        </div>
     </div>
-  </div>
-  <div class="py-16 bg-white dark:bg-gray-900">
-    <div class="max-w-5xl mx-auto">
-      <ol class="ml-5 relative border-l border-gray-200 dark:border-gray-700">
-        <li class="mb-10 ml-6" v-for="(item, index) in data.body" :key="index">
-          <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-gray-100 dark:ring-gray-800 dark:bg-blue-900">
-            <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor"
-              viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clip-rule="evenodd"></path>
-            </svg>
-          </span>
-          <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-            {{ item.title }}
-            <span v-if="item.latest" class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span>
-          </h3>
-          <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            Released on {{ item.date }}
-          </time>
-          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-            {{ item.description }}
-          </p>
-        </li>
-      </ol>
+    <div class="py-16 bg-white dark:bg-gray-900">
+        <div class="max-w-5xl mx-auto">
+        <ol class="ml-5 relative border-l border-gray-200 dark:border-gray-700">
+            <li class="mb-10 ml-6" v-for="(item, index) in data.body" :key="index">
+            <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-gray-100 dark:ring-gray-800 dark:bg-blue-900">
+                <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor"
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                    clip-rule="evenodd"></path>
+                </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                {{ item.title }}
+                <span v-if="item.latest" class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span>
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                Released on {{ item.date }}
+            </time>
+            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                {{ item.description }}
+            </p>
+            </li>
+        </ol>
+        </div>
     </div>
   </div>
 </template>
