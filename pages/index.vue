1657dba759db263184013a412f07d463477ecf63
Zakir Hossen <web.zakirbd@gmail.com>
2022-09-08 12:20:20 +0600

    content: ariful's testimonial added

--- pages/index.vue
+++ pages/index.vue
@@ -83,14 +83,21 @@
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 dark:text-gray-500 mb-4" viewBox="0 0 975.036 975.036">
                 <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
               </svg>
-              <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
-              <a class="inline-flex items-center">
-                <img alt="testimonial" src="https://dummyimage.com/106x106" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
-                <span class="flex-grow flex flex-col pl-4">
-                  <span class="title-font font-medium text-gray-900 dark:text-white">Ariful Islam</span>
-                  <span class="text-gray-500 text-sm">Laravel & Vuejs Developer</span>
-                </span>
-              </a>
+              <p class="leading-relaxed mb-6">
+                ২ বছর যাবত কাজ করতেছি, এত দিনে আমি যা যা স্কিল এবং অবিজ্ঞতা অর্জন করছি, সেইগুলুর উপর আমি সন্তুষ্ট। কাজের পাশাপাশি আমি আমার সাইড প্রোজেক্ট এ কাজ করতেছি, সেখানে আমি আমার স্কিল এবং অবিজ্ঞতা গুলো কাজে লাগাতে পারছি।
+              </p>
+              <div class="flex justify-between items-center">
+                <a class="inline-flex items-center">
+                  <img alt="testimonial" src="/testimonial/ariful-islam.png" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+                  <span class="flex-grow flex flex-col pl-4">
+                  <span class="title-font font-medium text-gray-900 dark:text-white">
+                    Ariful Islam
+                  </span>
+                  <span class="text-gray-500 text-sm">Full-stack Developer</span>
+                  </span>
+                </a>
+                <a href="https://youtu.be/snFXSKS6mrY" target="_blank" class="bg-blue-200 text-gray-700 rounded text-sm text-center px-2 py-1">Watch Video </a>
+              </div>
             </div>
           </div>
           <div class="p-4 md:w-1/2 w-full">
@@ -99,7 +106,7 @@
                 <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
               </svg>
               <p class="leading-relaxed mb-6">
-                যখনই কোন কিছুতে ঘাবড়াইয়া যাইতাম, আটকে যাইতাম তখনি জাকির ভাই আমাদের সাহস দিত, ভরসা দিয়ে দেখাইয়া দিত আএইভাবে এইভাবে করে ফেলবেন। দেখা গেছে, আমার সাহস এত বেড়ে গেছে এখন মনে হয় আমি পারবো। জাকির ভাই গাইডলাইন খুব ভাল দেয়।
+                যখনই কোন কিছুতে ঘাবড়াইয়া যাইতাম, আটকে যাইতাম তখনই জাকির ভাই আমাদের সাহস দিত, ভরসা দিয়ে দেখিয়ে দিত এইভাবে এইভাবে করে ফেলবেন। দেখা গেছে, আমার সাহস এত বেড়ে গেছে এখন মনে হয় আমি পারবো। আর ভাই গাইডলাইন খুব ভাল দেয়।
               </p>
               <div class="flex justify-between items-center">
                 <a class="inline-flex items-center">
